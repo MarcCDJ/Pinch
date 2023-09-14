d4606bf590b6f45fa35550d983c0837cd63b35de
MarcCDJ <marccruzdj@gmail.com>
2023-09-14 10:25:10 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Marc Cruz on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
