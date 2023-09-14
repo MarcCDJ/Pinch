d639e457ca96c82e684d612787cf558b12c3a734
MarcCDJ <marccruzdj@gmail.com>
2023-09-14 11:58:07 -0400

    Initial project setup

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Marc Cruz on 9/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
