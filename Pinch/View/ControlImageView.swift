f7d392d1613fbad26bb4693897e9ca4c26cbe75e
MarcCDJ <marccruzdj@gmail.com>
2023-09-25 10:08:11 -0400

    Add zoom buttons

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,24 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Marc Cruz on 9/25/23.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+#Preview {
+    ControlImageView(icon: "minus.magnifyingglass")
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
